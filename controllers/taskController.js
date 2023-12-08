e5283ce17989f45b0686fcd5e1f85275aee04e0a
adewobiadetayo1@gmail.com <adewobiadetayo1@gmail.com>
2023-12-08 17:12:22 +0100

    first commit

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -45,7 +45,41 @@ const getUserTask = async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+const updateTask = async (req, res) => {
+  const taskId = req.params.id;
+  const { title, description, dueDate } = req.body;
+
+  try {
+    const userId = req.user.id;
+    const [userTask] = await db.query("SELECT * FROM task WHERE id = ?", [
+      taskId,
+    ]);
+    if (userTask.length === 0) {
+      return res
+        .status(404)
+        .json({ success: true, message: "Task not found!" });
+    }
+    const result = await db.query(
+      "UPDATE task SET title = ?, description = ?, dueDate = ? WHERE id = ? AND user_id = ?",
+      [title, description, dueDate, taskId, userId]
+    );
+    if (result[0].affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Task not found or unauthorized" });
+    }
+
+    return res.status(200).json({ success: true, message: "Task updated" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Something went wrong" });
+  }
+};
+
 module.exports = {
   createTask,
   getUserTask,
+  updateTask,
 };
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 const router = Router();
-const { createTask, getUserTask } = require("../controllers/taskController");
+const {
+  createTask,
+  getUserTask,
+  updateTask,
+} = require("../controllers/taskController");
 const { verifyToken } = require("../middleware/auth");
 
 router.route("/").post(verifyToken, createTask).get(verifyToken, getUserTask);
+router.route("/:id").put(verifyToken, updateTask);
 
 module.exports = router;
